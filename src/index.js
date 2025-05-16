cee875d495d4121455e1124d5cb3bbf64b94ef17
CrzxaExe <craserchannel@outlook.co.id>
2025-05-16 16:04:11 +0700

    completing stats section

--- src/index.js
+++ src/index.js
@@ -12,13 +12,98 @@ const place = document.getElementById("place");
 const content = {
   t1: `<h1 class="mt-4 text-2xl lg:text-3xl">Talent: Mon3tr</h1>
   
-  <p class="mt-2 text-base lg:text-lg bg-[#242424] px-4 py-2 rounded-lg">Can use and heal Mon3tr, and Mon3tr has a 25 second redeployment time. Kal'tsit will prioritize healing herself and Mon3tr; If Mon3tr is not within Kal'tsit's Attack Range, Mon3tr's DEF is reduced to 0</p>`,
+  <p class="mt-2 text-base lg:text-lg bg-[#242424] z-2 px-4 py-2 rounded-lg max-w-[45em]">Can use and heal <span class="text-sky-400">Mon3tr</span>, and Mon3tr has a 25 second redeployment time. Kal'tsit will prioritize healing herself and Mon3tr; If Mon3tr is not within Kal'tsit's Attack Range, Mon3tr's DEF is reduced to 0</p>
+  
+  <h1 class="mt-4 text-2xl lg:text-3xl">Summon: Mon3tr</h1>
+
+  <span class="text-lg lg:text-xl mt-2 block">Icon</span>
+
+  <div class="flex flex-col lg:flex-row w-full min-h-fit gap-2">
+    <div class="flex flex-col max-w-[20rem] w-full lg:w-[20%]">
+      <div class="flex flex-row max-w-[20rem] w-full gap-2 z-1">
+        <img src="https://arknights.wiki.gg/images/3/33/Mon3tr_summon.png?f722c5" alt="Mon3tr" class="aspect-square w-[30%] lg:w-[50%] p-2 max-w-[8em] border-[0.5px] border-white rounded-lg" />
+        <img src="https://arknights.wiki.gg/images/b/b9/Mon3tr_summon_Skin_1.png?860947" alt="Mon3tr Remnant" class="aspect-square w-[30%] lg:w-[50%] max-w-[8em] border-[0.5px] border-white rounded-lg" />
+      </div>
+      <h1 class="mt-5">Range</h1>
+      <div class="flex h-[200%] max-h-[12em] w-full lg:justify-center items-center">
+        <div class="grid grid-cols-2 aspect-[2/1] gap-1 w-1/4 mt-3">
+          <span class="aspect-square p-[0.125em] block border-[#27a6f3] bg-[#27a6f3] border-[2px]"></span>
+          <span class="aspect-square p-[0.125em] block border-gray-400 border-[2px]"></span>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-full min-h-full flex flex-col gap-2">
+      <span
+        class="block bg-linear-to-r from-white/13 via-white/13 to-white/0 to-80% relative overflow-hidden rounded-sm px-4 before:content-[''] before:rounded-r-sm before:-ml-4 before:bg-white/10 before:absolute before:w-[86%] before:min-h-full before:py-0.5 before:px-2 text-white font-mono"
+        >HP : 5433</span
+      >
+      <span
+        class="block bg-linear-to-r from-white/13 via-white/13 to-white/0 to-80% relative overflow-hidden rounded-sm px-4 before:content-[''] before:rounded-r-sm before:-ml-4 before:bg-white/10 before:absolute before:w-[74%] before:min-h-full before:py-0.5 before:px-2 text-white font-mono"
+        >Atk : 1402</span
+      >
+      <span
+        class="block bg-linear-to-r from-white/13 via-white/13 to-white/0 to-80% relative overflow-hidden rounded-sm px-4 before:content-[''] before:rounded-r-sm before:-ml-4 before:bg-white/10 before:absolute before:w-[38%] before:min-h-full before:py-0.5 before:px-2 text-white font-mono"
+        >Def : 405
+      </span>
+      <span
+        class="block bg-linear-to-r from-white/13 via-white/13 to-white/0 to-80% relative overflow-hidden rounded-sm px-4 before:content-[''] before:rounded-r-sm before:-ml-4 before:bg-white/10 before:absolute before:w-[0%] before:min-h-full before:py-0.5 before:px-0.5 text-white font-mono"
+        >Res : 0
+      </span>
+      <span
+        class="block bg-linear-to-r from-white/13 via-white/13 to-white/0 to-80% relative overflow-hidden rounded-sm px-4 before:content-[''] before:rounded-r-sm before:-ml-4 before:bg-white/10 before:absolute before:w-[40%] before:min-h-full before:py-0.5 before:px-2 text-white font-mono"
+        >Redeploy : 25s
+      </span>
+      <span
+        class="block bg-linear-to-r from-white/13 via-white/13 to-white/0 to-80% relative overflow-hidden rounded-sm px-4 before:content-[''] before:rounded-r-sm before:-ml-4 before:bg-white/10 before:absolute before:w-[20%] before:min-h-full before:py-0.5 before:px-2 text-white font-mono"
+        >DP Cost : 10
+      </span>
+      <span
+        class="block bg-linear-to-r from-white/13 via-white/13 to-white/0 to-80% relative overflow-hidden rounded-sm px-4 before:content-[''] before:rounded-r-sm before:-ml-4 before:bg-white/10 before:absolute before:w-[75%] before:min-h-full before:py-0.5 before:px-2 text-white font-mono"
+        >Block : 3
+      </span>
+      <span
+        class="block bg-linear-to-r from-white/13 via-white/13 to-white/0 to-80% relative overflow-hidden rounded-sm px-4 before:content-[''] before:rounded-r-sm before:-ml-4 before:bg-white/10 before:absolute before:w-[38%] before:min-h-full before:py-0.5 before:px-2 text-white font-mono"
+        >Attack Interval : 2s
+      </span>
+    </div>
+  </div>
+
+  <div class="absolute -top-5 -right-8 2xl:-right-4 aspect-[16/7.4] 2xl:aspect-[12/7.5] overflow-hidden min-h-[106%] bg-slate-500 flex justify-end items-start select-none -z-[6]">
+    <div class="w-full min-h-full bg-linear-to-r from-[#545557]/100 from-60% lg:form-40% via-[#545557]/98 lg:via-[#545557]/80 via-95% lg:via-80% to-[#545557]/0 absolute -left-[19em] -z-[1]"></div>
+    <img src="./src/img/mon3tr.png" alt="mon3tr-art" class="opacity-70 aspect-square object-cover max-w-[89em] min-h-full sm:min-h-fit sm:max-w-[51em] relative xl:-mr-14 -z-[2]" />
+  </div>`,
 
   t2: `<h1 class="mt-4 text-2xl lg:text-3xl">Talent: Non-Damaging Restructuring</h1>
   
-  <p class="mt-2 text-base lg:text-lg bg-[#242424] px-4 py-2 rounded-lg">When Mon3tr is defeated (excluding retreat), all enemies in the surrounding 8 tiles are Stunned for 3.5 (+0.5) seconds and receive 1400 (+200) True damage</p>`,
+  <p class="mt-2 text-base lg:text-lg bg-[#242424] px-4 py-2 rounded-lg max-w-[45em]">When Mon3tr is defeated (excluding retreat), all enemies in the surrounding 8 tiles are Stunned for 3.5 <span class="text-sky-400">(+0.5)</span> seconds and receive 1400 <span class="text-sky-400">(+200)</span> True damage</p>
+  
+  <h1 class="mt-5">Range</h1>
+  <div class="flex w-full aspect-square max-w-[10em] lg:justify-center items-center -mt-2">
+    <div class="grid grid-cols-3 aspect-[2/1] gap-1 w-2/4 mt-3">
+      <span class="aspect-square p-[0.125em] block border-gray-400 border-[2px]"></span>
+      <span class="aspect-square p-[0.125em] block border-gray-400 border-[2px]"></span>
+      <span class="aspect-square p-[0.125em] block border-gray-400 border-[2px]"></span>
+
+      <span class="aspect-square p-[0.125em] block border-gray-400 border-[2px]"></span>
+      <span class="aspect-square p-[0.125em] block border-[#27a6f3] bg-[#27a6f3] border-[2px]"></span>
+      <span class="aspect-square p-[0.125em] block border-gray-400 border-[2px]"></span>
+
+      <span class="aspect-square p-[0.125em] block border-gray-400 border-[2px]"></span>
+      <span class="aspect-square p-[0.125em] block border-gray-400 border-[2px]"></span>
+      <span class="aspect-square p-[0.125em] block border-gray-400 border-[2px]"></span>
+    </div>
+  </div>
+
+  <div class="absolute -top-5 -right-8 2xl:-right-4 aspect-[16/7.4] 2xl:aspect-[12/7.5] overflow-hidden min-h-[106%] bg-slate-500 flex justify-end items-start select-none -z-[6]">
+    <div class="w-full min-h-full bg-linear-to-r from-[#545557]/100 from-60% lg:form-40% via-[#545557]/98 lg:via-[#545557]/80 via-95% lg:via-80% to-[#545557]/0 absolute -left-[19em] -z-[1]"></div>
+    <img src="./src/img/mon3tr.png" alt="mon3tr-art" class="opacity-70 aspect-square object-cover max-w-[51em] relative xl:-mr-14 -z-[2]" />
+  </div>`,
 
   s1: `<h1 class="mt-4 text-2xl lg:text-3xl w-full">Skill: Command: Structural Fortification </h1>
+
+  <img src="https://arknights.wiki.gg/images/thumb/0/05/Rank_10.png/30px-Rank_10.png?5a915c" alt="m3" />
+
   <div class="flex flex-col lg:flex-row gap-2 w-full mt-4">
     <div class="lg:w-[7.6%] min-h-full">
         <img
@@ -28,14 +113,14 @@ const content = {
         />
     </div>
     <div class="w-full lg:w-[92.4%] mt-3 lg:mt-0">
-        <div class="bg-[#242424] px-4 py-2 text-base lg:text-lg flex flex-col rounded-lg">
-            <span>Auto Recovery</span>
-            <span>Manual</span>
+        <div class="bg-[#242424] px-4 py-2 text-base lg:text-lg flex flex-col rounded-lg gap-1">
+            <span class="px-2 py-1 rounded-lg bg-lime-600 inline w-fit">Auto Recovery</span>
+            <span  class="px-2 py-1 rounded-lg bg-gray-400 inline w-fit">Manual</span>
             <span>SP: 10 Initial/20 Sp, 40s duration</span>
         </div>
 
         <p class="mt-5 text-lg lg:text-xl bg-[#242424] px-4 py-2 rounded-lg">
-        This unit and Mon3tr gains +150% DEF. This unit also gains 50% Physical resist
+        This unit and Mon3tr gains <span class="text-sky-400">+150%</span> DEF. This unit also gains <span class="text-sky-400">50%</span> Physical resist
         </p>
     </div>
   </div>`,
@@ -50,15 +135,15 @@ const content = {
         />
     </div>
     <div class="w-full lg:w-[92.4%] mt-3 lg:mt-0">
-        <div class="bg-[#242424] px-4 py-2 text-base lg:text-lg flex flex-col rounded-lg">
-            <span>Auto Recovery</span>
-            <span>Manual</span>
+        <div class="bg-[#242424] px-4 py-2 text-base lg:text-lg flex flex-col rounded-lg gap-1">
+            <span class="px-2 py-1 rounded-lg bg-lime-600 inline w-fit">Auto Recovery</span>
+            <span class="px-2 py-1 rounded-lg bg-gray-400 inline w-fit">Manual</span>
             <span>SP: 0 Initial/8 SP, 20s duration</span>
         </div>
 
         <p class="mt-5 text-lg lg:text-xl bg-[#242424] px-4 py-2 rounded-lg">
-        This unit gains +100 ASPD; Mon3tr gains ATK +90% and attacks all blocked enemies
-        This skill is Tied to Mon3tr
+        This unit gains <span class="text-sky-400">+100</span> ASPD; Mon3tr gains ATK <span class="text-sky-400">+90%</span> and attacks all blocked enemies
+        This skill is <span class="text-amber-400">Tied</span> to Mon3tr
         </p>
     </div>
   </div>`,
@@ -73,14 +158,14 @@ const content = {
         />
     </div>
     <div class="w-full lg:w-[92.4%] mt-3 lg:mt-0">
-        <div class="bg-[#242424] px-4 py-2 text-base lg:text-lg flex flex-col rounded-lg">
-            <span>Auto Recovery</span>
-            <span>Manual</span>
+        <div class="bg-[#242424] px-4 py-2 text-base lg:text-lg flex flex-col rounded-lg gap-1">
+            <span class="px-2 py-1 rounded-lg bg-lime-600 inline w-fit">Auto Recovery</span>
+            <span class="px-2 py-1 rounded-lg bg-gray-400 inline w-fit">Manual</span>
             <span>SP: 0 Initial/15 SP, 20s duration</span>
         </div>
 
         <p class="mt-5 text-lg lg:text-xl bg-[#242424] px-4 py-2 rounded-lg">
-        Mon3tr gains DEF +200%, ATK +260% that gradually decays to +0% over the skill duration and deals True damage. If this skill ends without Mon3tr killing any enemies, Mon3tr loses 50% of Max HP. This skill is Tied to Mon3tr
+        Mon3tr gains DEF <span class="text-sky-400">+200%</span>, ATK <span class="text-sky-400">+260%</span> that gradually decays to +0% over the skill duration and deals <span class="text-sky-400">True</span> damage. If this skill ends without Mon3tr killing any enemies, Mon3tr loses <span class="text-sky-400">50%</span> of Max HP. This skill is <span class="text-amber-400">Tied</span> to Mon3tr
         </p>
     </div>
   </div>`,
@@ -97,6 +182,7 @@ t2.addEventListener("click", () => {
 });
 
 s1.addEventListener("click", () => {
+  console.log("test");
   place.innerHTML = content.s1;
 });
 s2.addEventListener("click", () => {
